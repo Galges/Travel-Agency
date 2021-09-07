1e64397b415f175ecb5fcb1ae6f6e87f13d5c5f1
Mateusz Gałuszka <galuszka1996@gmail.com>
2021-09-08 00:00:48 +0200

    fix views and filters

--- src/components/features/TripListOptions/TripListOptionsContainer.js
+++ src/components/features/TripListOptions/TripListOptionsContainer.js
@@ -1,16 +1,27 @@
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import TripListOptions from './TripListOptions';
-import {getAllTags} from '../../../redux/tagsRedux';
-import {getAllFilters, changeSearchPhrase} from '../../../redux/filtersRedux';
+import { getAllTags } from '../../../redux/tagsRedux';
+import {
+  getAllFilters,
+  changeSearchPhrase,
+  addTag,
+  removeTag,
+  changeFromDuration,
+  changeToDuration,
+} from '../../../redux/filtersRedux';
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   tags: getAllTags(state),
   filters: getAllFilters(state),
 });
 
-const mapDispatchToProps = dispatch => ({
-  changeSearchPhrase: phrase => dispatch(changeSearchPhrase(phrase)),
-  // TODO - add more dispatchers for other filters
+const mapDispatchToProps = (dispatch) => ({
+  changeSearchPhrase: (phrase) => dispatch(changeSearchPhrase(phrase)),
+  addTag: (tag) => dispatch(addTag(tag)),
+  removeTag: (tag) => dispatch(removeTag(tag)),
+  changeFromDuration: (value) => dispatch(changeFromDuration(value)),
+  changeToDuration: (value) => dispatch(changeToDuration(value)),
+  // TODO - add more dispatchers for other filters - OK
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TripListOptions);
--- src/redux/filtersRedux.js
+++ src/redux/filtersRedux.js
@@ -1,20 +1,38 @@
 /* SELECTORS */
 
-export const getAllFilters = ({filters}) => filters;
+export const getAllFilters = ({ filters }) => filters;
 
 /* ACTIONS */
 
 // action name creator
 const reducerName = 'filters';
-const createActionName = name => `app/${reducerName}/${name}`;
+const createActionName = (name) => `app/${reducerName}/${name}`;
 
 // action types
 export const CHANGE_PHRASE = createActionName('CHANGE_PHRASE');
-// TODO - add other action types
+export const ADD_TAG = createActionName('ADD_TAGS');
+export const REMOVE_TAG = createActionName('REMOVE_TAGS');
+export const CHANGE_FROM_DURATION = createActionName('CHANGE_FROM_DURATION');
+export const CHANGE_TO_DURATION = createActionName('CHANGE_TO_DURATION');
+// TODO - add other action types - OK
 
 // action creators
-export const changeSearchPhrase = payload => ({ payload, type: CHANGE_PHRASE });
-// TODO - add other action creators
+export const changeSearchPhrase = (payload) => ({
+  payload,
+  type: CHANGE_PHRASE,
+});
+export const addTag = (payload) => ({ payload, type: ADD_TAG });
+export const removeTag = (payload) => ({ payload, type: REMOVE_TAG });
+export const changeFromDuration = (payload) => ({
+  payload,
+  type: CHANGE_FROM_DURATION,
+});
+export const changeToDuration = (payload) => ({
+  payload,
+  type: CHANGE_TO_DURATION,
+});
+
+// TODO - add other action creators - OK
 
 // reducer
 export default function reducer(statePart = [], action = {}) {
@@ -24,7 +42,33 @@ export default function reducer(statePart = [], action = {}) {
         ...statePart,
         searchPhrase: action.payload,
       };
-    // TODO - handle other action types
+    case ADD_TAG:
+      return {
+        ...statePart,
+        tags: [...statePart.tags, action.payload],
+      };
+    case REMOVE_TAG:
+      return {
+        ...statePart,
+        tags: [...statePart.tags.filter((tag) => tag !== action.payload)],
+      };
+    case CHANGE_FROM_DURATION:
+      return {
+        ...statePart,
+        duration: {
+          ...statePart.duration,
+          from: action.payload,
+        },
+      };
+    case CHANGE_TO_DURATION:
+      return {
+        ...statePart,
+        duration: {
+          ...statePart.duration,
+          to: action.payload,
+        },
+      };
+    // TODO - handle other action types - OK
     default:
       return statePart;
   }
